f8cf7927d5beec59175659b02d28bc8ec3098a89
vc10hueb <vinsnwbrd@aol.com>
2013-10-03 15:47:05 -0400

    added cityRec

--- src/main/java/gov/nysenate/ams/provider/AMS.java
+++ src/main/java/gov/nysenate/ams/provider/AMS.java
@@ -1,4 +1,23 @@
 package gov.nysenate.ams.provider;
 
-public class AMS {
+import gov.nysenate.ams.service.AddressService;
+import gov.nysenate.ams.service.LicensingService;
+
+public class AMS implements LicensingService, AddressService {
+
+
+    @Override
+    public String getApiVersion() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int getDataExpireDays() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int getLibraryExpireDays() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
--- src/main/java/gov/nysenate/ams/service/AddressService.java
+++ src/main/java/gov/nysenate/ams/service/AddressService.java
@@ -1,10 +1,13 @@
 package gov.nysenate.ams.service;
 
+import gov.nysenate.ams.model.Address;
+import gov.nysenate.ams.model.USPSAddress;
+
 /**
  * This interface defines the necessary address validation/lookup functions that need to be
  * supported by the address management system.
  */
 public interface AddressService
 {
-
+    public USPSAddress validate(Address adress);
 }
