e899cfd1210d26eec4fa698a38827605907d2437
JacobKeegan <jacobmkeegan@gmail.com>
2025-06-20 01:07:30 -0400

    Removed an unused file, and an unused method.

--- src/main/java/gov/nysenate/ams/model/RecordType.java
+++ src/main/java/gov/nysenate/ams/model/RecordType.java
@@ -4,38 +4,17 @@ package gov.nysenate.ams.model;
  * Represents the type of record associated with an entry in an address record stack.
  */
 public enum RecordType {
-    F ("Firm",
-        "This is a match to a Firm Record, which is the finest level of match available for an address."),
-
-    G ("General Delivery",
-        "This is a match to a General Delivery record."),
-
-    H ("Building / Apartment",
-        "This is a match to a Building or Apartment record."),
-
-    P ("Post Office Box",
-        "This is a match to a Post Office Box."),
-
-    R ("Rural Route or Highway Contract",
-        "This is a match to either a Rural Route or a Highway Contract record, both of which may have " +
-        "associated Box Number ranges."),
-
-    S ("Street Record",
-        "This is a match to a Street record containing a valid primary number range.");
+    F("Firm"), G("General Delivery"),
+    H("Building / Apartment"), P("Post Office Box"),
+    R("Rural Route or Highway Contract"), S("Street Record");
 
     private final String shortDesc;
-    private final String longDesc;
 
-    RecordType(String shortDesc, String longDesc) {
+    RecordType(String shortDesc) {
         this.shortDesc = shortDesc;
-        this.longDesc = longDesc;
     }
 
     public String getShortDesc() {
         return shortDesc;
-    }
-
-    public String getLongDesc() {
-        return longDesc;
     }
 }
